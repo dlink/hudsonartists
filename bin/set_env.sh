05e62035ed07425deabb842594d240493f4861ff
David Link <dvlink@gmail.com>
2015-06-13 10:02:24 -0400

    New script: set_env.sh

---
+++ bin/set_env.sh
@@ -0,0 +1,6 @@
+source $HOME/hudsonartists/bin/aliases
+export VCONF=$HOME/hudsonartists/conf/dev.yml
+export PYTHONPATH=$HOME/hudsonartists/lib
+
+echo \$VCONF=$VCONF
+echo \$PYTHONPATH=$PYTHONPATH
